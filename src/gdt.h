004a2ea64ee27685faacded9244834e1a3c514c1
Dan Jenkins <jenkins.daniel.02@gmail.com>
2019-02-14 00:34:54 +0200

    [new] Started GDT
    Started the global descriptor table

---
+++ src/gdt.h
@@ -0,0 +1,29 @@
+#ifndef INCLUDE_GDT_H
+#define INCLUDE_GDT_H
+
+/**
+ * The GDT struct. This holds information on the global descriptor table,
+ * describing memory segmentation. In the operating system we have the
+ * following basic segments:
+ *
+ *  Index | Offset | Name                | Address range           | Type | DPL
+ * -------+--------+---------------------+-------------------------+------+-----
+ *    0   |  0x0   | null descriptor     |                         |      |
+ *    1   |  0x08  | kernel code segment | 0x00000000 - 0xFFFFFFFF |  RX  | PL0
+ *    2   |  0x10  | kernel data segment | 0x00000000 - 0xFFFFFFFF |  RW  | PL0
+ *
+ *
+ * We also have four priviledge levels, specified by the DPL field. These are
+ * laid out as follows:
+ *  - PL0: kernel
+ *  - PL1: operating system processes
+ *  - PL2: hardware drivers
+ *  - PL3: userspace applications
+ */
+struct gdt {
+	unsigned int address;
+	unsigned short size;
+} __attribute__((packed));
+
+
+#endif /* INCLUDE_GDT_H */
